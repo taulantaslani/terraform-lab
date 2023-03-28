06eae96be426d39f7649ba6ac64b74cb814fc7d7
taulantaslani <taulantaslani@gmail.com>
2023-03-28 20:17:52 +0200

    IAM User with profile and access key

---
+++ ec2/iam-user/main.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_user" "this" {
+  name = "taulantasllani"
+
+  tags = {
+   Infrastructure = "appstellar-training"
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect    = "Allow"
+    actions = [
+        "eks:ListNodegroups",
+        "eks:DescribeFargateProfile",
+        "eks:ListTagsForResource",
+        "eks:ListAddons",
+        "eks:DescribeAddon",
+        "eks:ListFargateProfiles",
+        "eks:DescribeNodegroup",
+        "eks:DescribeIdentityProviderConfig",
+        "eks:ListUpdates",
+        "eks:DescribeUpdate",
+        "eks:AccessKubernetesApi",
+        "eks:DescribeCluster",
+        "eks:ListClusters",
+        "eks:DescribeAddonVersions",
+        "eks:ListIdentityProviderConfigs"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions = ["iam:ChangePassword"]
+    resources = [aws_iam_user.this.arn]
+  }
+}
+
+resource "aws_iam_user_policy" "this" {
+  name   = "taulantasllani-policy"
+  user   = aws_iam_user.this.name
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_user_login_profile" "this" {
+  user    = aws_iam_user.this.name
+}
+
+resource "aws_iam_access_key" "this" {
+  user    = aws_iam_user.this.name
+}
---
+++ ec2/iam-user/outputs.tf
@@ -0,0 +1,12 @@
+output "password" {
+    value = aws_iam_user_login_profile.this.password
+}
+
+output "access_key" {
+    value = aws_iam_access_key.this.id
+}
+
+output "secret_key" {
+    value = aws_iam_access_key.this.secret
+    sensitive = true
+}
